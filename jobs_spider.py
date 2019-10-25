c7c4c6dd96ea1a7c1451e6fe29ae64823fed8da1
Marusa <marusa15@gmail.com>
2019-10-25 11:15:18 +0200

    First commit.

---
+++ jobs_spider.py
@@ -0,0 +1,24 @@
+import scrapy
+
+
+class JobsSpider(scrapy.Spider):
+    name = 'jobs'
+    start_urls = [
+        'https://slo-tech.com/delo',
+    ]
+
+    def parse(self, response):
+
+        for job in response.xpath('//div[@id="page_content"]/div[@id="content"]/table[@class="forums"]/tbody/tr'):
+        
+            yield {
+                'title': job.xpath('td[@class="name"]/h3/a/text()').get(),
+                'department': job.xpath('td[@class="name"]/div/span[@class="oddelek"]/a/text()').get(),
+                'poster': job.xpath('td[@class="company"]/a/text()').get(),
+            }
+
+      
+
+
+
+        
